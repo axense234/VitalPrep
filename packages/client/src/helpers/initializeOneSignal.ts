68af596e888432e67596e82c78a1497f3f37c98f
axense234 <andreicomanescuonline@gmail.com>
2024-04-29 21:47:14 +0300

    Changing a bit of stuff.

--- packages/client/src/helpers/initializeOneSignal.ts
+++ packages/client/src/helpers/initializeOneSignal.ts
@@ -4,7 +4,8 @@ import OneSignal from "react-onesignal";
 const initializeOneSignal = async () => {
   await OneSignal.init({
     appId: process.env.NEXT_PUBLIC_ONE_SIGNAL_APP_ID as string,
-    allowLocalhostAsSecureOrigin: true,
+    allowLocalhostAsSecureOrigin:
+      process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test",
     notifyButton: { enable: true },
   });
 };
